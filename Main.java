55eddddf7bd0c287864ac352e63c8c3de9b90a29
451lucca <luccaedoardodevasconcelos@gmail.com>
2026-06-15 00:35:49 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        LegacyLogger legacy = new LegacyLogger();
-        Logger logger = new LegacyLoggerAdapter(legacy);
-        Application app = new Application(logger);
-
-        app.run();
-    }
-}
